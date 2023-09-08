6f2b3debd6a63e58aa2f5e5b7f4cc707838177a1
sunandhini96 <63030539+sunandhini96@users.noreply.github.com>
2023-09-09 02:46:30 +0530

    Update config.py

--- Session_16/config.py
+++ Session_16/config.py
@@ -4,15 +4,15 @@ from pathlib import Path
 def get_config():
     return {
         "batch_size": 16,
-        "num_epochs": 10,
-        "lr": 10**-4,
-        "seq_len": 350,
+        "num_epochs": 20,
+        "lr": 10**-3,
+        "seq_len": 180,
         "d_model": 512,
         "lang_src": "en",
-        "lang_tgt": "it",
+        "lang_tgt": "fr",
         "model_folder": "weights",
         "model_basename": "tmodel_",
-        "preload": True,
+        "preload": False,
         "tokenizer_file": "tokenizer_{0}.json",
         "experiment_name": "runs/tmodel",
     }
