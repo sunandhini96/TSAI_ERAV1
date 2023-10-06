e449633ec834db4b043a2a64a2e2bc33846d3a2e
sunandhini96 <63030539+sunandhini96@users.noreply.github.com>
2023-10-06 13:13:14 +0530

    Update unet_model.py

--- Session_18/UNet/unet_model.py
+++ Session_18/UNet/unet_model.py
@@ -47,11 +47,11 @@ class ContractingBlock(nn.Module):
     def __init__(self, in_channels, out_channels,pool="none"):
         super(ContractingBlock, self).__init__()
 
-       self.double_conv=DoubleConv(in_channels,out_channels)
+        self.double_conv=DoubleConv(in_channels,out_channels)
 
         self.pool_layer = get_pooling_layer(pool,out_channels)
 
-     def DoubleConv(self,in_channels,out_channels):
+    def DoubleConv(self,in_channels,out_channels):
        return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
@@ -61,7 +61,7 @@ class ContractingBlock(nn.Module):
             nn.ReLU(inplace=True)
         )
        
-      def get_pooling_layer(self,pool,out_cannels):
+    def get_pooling_layer(self,pool,out_cannels):
         if pool == "max":
           return nn.MaxPool2d(kernel_size=2, stride=2)
         elif pool== "stridedconv":
@@ -85,7 +85,7 @@ class ExpandingBlock(nn.Module):
         self.upsample = get_upsampling_layer(in_channels,out_channels,up_sample)
         self.double_conv=DoubleConv(in_channels,out_channels)
 
-     def DoubleConv(self,in_channels,out_channels):
+    def DoubleConv(self,in_channels,out_channels):
        return nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.BatchNorm2d(out_channels),
@@ -95,7 +95,7 @@ class ExpandingBlock(nn.Module):
             nn.ReLU(inplace=True)
         )
     def get_upsampling_layer(self,in_channels,out_channels,up_sample):
-      if up_sample="upsample":
+      if up_sample=="upsample":
         return nn.Sequential(
             nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False),
             nn.Conv2d(in_channels, in_channels//2, kernel_size=1)
@@ -152,6 +152,5 @@ class UNet(nn.Module):
         x= self.final_conv(x)
         #x=self.sigmoid(x)
         return x 
-
 
 
