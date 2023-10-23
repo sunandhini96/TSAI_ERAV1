1a3e7970e46ebeda09cc2a91663250755de6193f
sunandhini96 <63030539+sunandhini96@users.noreply.github.com>
2023-10-23 07:19:08 +0530

    Update utils.py

--- Session_20/utils.py
+++ Session_20/utils.py
@@ -15,41 +15,12 @@ from torchvision import transforms as tfms
 from tqdm.auto import tqdm
 from transformers import CLIPTextModel, CLIPTokenizer, logging
 import os
-from device import torch_device
-
-# Load the autoencoder model which will be used to decode the latents into image space.
-vae = AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae")
-
-# Load the tokenizer and text encoder to tokenize and encode the text.
-tokenizer = CLIPTokenizer.from_pretrained("openai/clip-vit-large-patch14")
-text_encoder = CLIPTextModel.from_pretrained("openai/clip-vit-large-patch14")
-
-# The UNet model for generating the latents.
-unet = UNet2DConditionModel.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="unet")
-
-# The noise scheduler
-scheduler = LMSDiscreteScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", num_train_timesteps=1000)
-torch_device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
-# To the GPU we go!
-vae = vae.to(torch_device)
-text_encoder = text_encoder.to(torch_device)
-unet = unet.to(torch_device);
+from device import torc_device,vae,text_encoder,unet,tokenizer,scheduler,token_emb_layer,pos_emb_layer,position_embeddings
 
 
-token_emb_layer = text_encoder.text_model.embeddings.token_embedding
-pos_emb_layer = text_encoder.text_model.embeddings.position_embedding
-position_ids = text_encoder.text_model.embeddings.position_ids[:, :77]
-position_embeddings = pos_emb_layer(position_ids)
-
-torch.manual_seed(1)
-if not (Path.home()/'.cache/huggingface'/'token').exists(): notebook_login()
 
 # Supress some unnecessary warnings when loading the CLIPTextModel
 logging.set_verbosity_error()
-
-# Set device
-torch_device = "cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu"
-if "mps" == torch_device: os.environ['PYTORCH_ENABLE_MPS_FALLBACK'] = "1"
 
 def pil_to_latent(input_im):
     # Single image -> single latent in a batch (so size 1, 4, 64, 64)
