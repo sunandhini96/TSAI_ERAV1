047a5e64d796378c664c43515bbcf9181b151a43
Sunandini <63030539+sunandhini96@users.noreply.github.com>
2024-02-12 23:42:50 +0530

    Update finetune_dataset.py

--- Capstone/Stage_2/finetune_dataset.py
+++ Capstone/Stage_2/finetune_dataset.py
@@ -12,32 +12,26 @@ os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 
 class llavadataset(Dataset):
-  def __init__(self, qa_dataset, phi_model_name, clip_model_name, tokenizer,processor):
-
-    self.tokenizer  = tokenizer
-    self.processor  = processor
+  def __init__(self, qa_dataset, phi_model_name, clip_model_name, tokenizer):
+    self.processor  = AutoProcessor.from_pretrained(clip_model_name)
     self.qa_dataset = qa_dataset
 
   def __len__(self):
     return self.qa_dataset.shape[0]
 
   def __getitem__(self, idx):
-
+    # from image perspective
     img_url = self.qa_dataset.img_url[idx]
-    ques = self.qa_dataset.input[idx]
-    ans = self.qa_dataset.label[idx]
+    ques    = torch.tensor(np.array(np.matrix(self.qa_dataset.input[idx]))[0])  
+    ans     = torch.tensor(np.array(np.matrix(self.qa_dataset.label[idx]))[0])
     
-#     ques    = torch.tensor(np.array(np.matrix(self.qa_dataset.input[idx]))[0])  
-#     ans     = torch.tensor(np.array(np.matrix(self.qa_dataset.label[idx]))[0])
-
     # image load
     image_load = Image.open(requests.get(img_url,stream=True).raw)
     image_processed = self.processor(images=image_load, return_tensors="pt") ['pixel_values']
     image_processed = image_processed.squeeze(0)
-    q = self.tokenizer(ques, return_tensors="pt", return_attention_mask=False)['input_ids'].squeeze(0)
-    a = self.tokenizer(ans, return_tensors="pt", return_attention_mask=False)['input_ids'].squeeze(0)
-
-    return(image_processed , q, a)
+    # q = self.tokenizer(ques, return_tensors="pt", return_attention_mask=False)['input_ids'].squeeze(0)
+    # a = self.tokenizer(ans, return_tensors="pt", return_attention_mask=False)['input_ids'].squeeze(0)
+    return(image_processed , ques, ans)
   
 
 def collate_fn(batch):
